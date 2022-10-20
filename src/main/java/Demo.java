5cb00022778a8145b5a0b1d07d89080a13d3bb9f
prefess <maicotion@gmail.com>
2022-10-20 23:31:53 +0700

    creational bridge

---
+++ src/main/java/Demo.java
@@ -0,0 +1,36 @@
+
+import devices.Device;
+import devices.Radio;
+import devices.Tv;
+import remotes.AdvancedRemote;
+import remotes.BasicRemote;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Thoai
+ */
+public class Demo {
+    public static void main(String[] args) {
+        testDevice(new Tv());
+        testDevice(new Radio());
+    }
+
+    public static void testDevice(Device device) {
+        System.out.println("Tests with basic remote.");
+        BasicRemote basicRemote = new BasicRemote(device);
+        basicRemote.power();
+        device.printStatus();
+
+        System.out.println("Tests with advanced remote.");
+        AdvancedRemote advancedRemote = new AdvancedRemote(device);
+        advancedRemote.power();
+        advancedRemote.mute();
+        device.printStatus();
+    }
+}
